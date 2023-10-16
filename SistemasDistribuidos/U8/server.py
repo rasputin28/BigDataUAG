3533139e09f5f988c4871e88089efbae7451b32d
root <root@LAPTOP-T4GAVNGS>
2023-10-16 11:02:02 -0500

    u8

--- SistemasDistribuidos/U8/server.py
+++ SistemasDistribuidos/U8/server.py
@@ -1,5 +1,3 @@
-# This code is for the server 
-# Lets import the libraries
 import socket
 import cv2
 import pickle
@@ -7,34 +5,36 @@ import struct
 import imutils
 
 # Socket Create
-server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-host_name  = socket.gethostname()
-host_ip = socket.gethostbyname(host_name)
-print('HOST IP:',host_ip)
-port = 9999
-socket_address = (host_ip,port)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket_address = ('127.0.0.0', 9999)
 
 # Socket Bind
 server_socket.bind(socket_address)
 
 # Socket Listen
 server_socket.listen(5)
-print("LISTENING AT:",socket_address)
+print("LISTENING AT:", socket_address)
 
-# Socket Accept
 while True:
-	client_socket,addr = server_socket.accept()
-	print('GOT CONNECTION FROM:',addr)
-	if client_socket:
-		vid = cv2.VideoCapture(0)
-		
-		while(vid.isOpened()):
-			img,frame = vid.read()
-			frame = imutils.resize(frame,width=320)
-			a = pickle.dumps(frame)
-			message = struct.pack("Q",len(a))+a
-			client_socket.sendall(message)
-			
-			cv2.imshow('TRANSMITTING VIDEO',frame)
-			if cv2.waitKey(1) == '13':
-				client_socket.close()
+    client_socket, addr = server_socket.accept()
+    print('GOT CONNECTION FROM:', addr)
+    if client_socket:
+        vid = cv2.VideoCapture(0)
+
+        while vid.isOpened():
+            img, frame = vid.read()
+            frame = imutils.resize(frame, width=320)
+            a = pickle.dumps(frame)
+            message = struct.pack("Q", len(a)) + a
+            client_socket.sendall(message)
+
+            cv2.imshow('TRANSMITTING VIDEO', frame)
+
+            # Break the loop if the Enter key is pressed or if the client disconnects
+            if cv2.waitKey(1) == 13 or not client_socket:
+                break
+
+        # Release resources and close the client socket
+        vid.release()
+        cv2.destroyAllWindows()
+        client_socket.close()
